d27097bedf888d01bd398088a73f9ca5ac2c695c
Jesus Vilchis <jesus.vilchis.ipn@gmail.com>
2024-02-19 18:38:02 -0600

    Prueba para produccion

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:1337"
+export const API_URL = "https://strapi-production-ff3e.up.railway.app"
 export const TOKEN = "token";
 export const PRODUCTS = "products";
